9a028d9fec5aa4a4e640f8ba9702c475a6d4f630
max-carlo <max.carlo@mailbox.org>
2025-03-18 14:23:13 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-streamlit run momentum.py --server.port=${PORT:-8501} --server.address=0.0.0.0
+streamlit run momentum.py --server.port=${PORT:-10000} --server.address=0.0.0.0
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome/"
