402b08d802645ece627445098a3eff20711533d7
max-carlo <max.carlo@mailbox.org>
2025-03-18 14:04:11 +0100

    Update momentum.py

--- momentum.py
+++ momentum.py
@@ -6,14 +6,14 @@ import time
 @st.cache_data
 def get_earnings_data(ticker):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.binary_location = "/opt/render/project/.render/chrome/opt/google/chrome/chrome"
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    # Pfad explizit setzen
-    chrome_options.binary_location = "/usr/bin/google-chrome"
-
-    driver = webdriver.Chrome(options=chrome_options)
+    service = webdriver.ChromeService(executable_path="/opt/render/project/.render/chrome/usr/local/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     url = f"https://finance.yahoo.com/quote/{ticker}/earnings"
     driver.get(url)
     time.sleep(5)
