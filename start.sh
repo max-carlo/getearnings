c64a912b81e3d2c7488a2e20aff86ac593d5ba86
max-carlo <max.carlo@mailbox.org>
2025-03-18 14:47:53 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Chrome-Pfad setzen (vorher!)
-export PATH="/opt/render/project/chrome/opt/google/chrome:$PATH"
-
-# Jetzt Streamlit starten
-streamlit run momentum.py --server.port=${PORT:-10000} --server.address=0.0.0.0
