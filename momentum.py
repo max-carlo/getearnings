68dc847539cf7a9ed45e4bac8326be967212d5b2
max-carlo <max.carlo@mailbox.org>
2025-03-19 21:11:41 +0100

    Update momentum.py

--- momentum.py
+++ momentum.py
@@ -9,10 +9,10 @@ def get_earnings_data(ticker):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/google-chrome"
+    chrome_options.binary_location = os.getenv("CHROME_BIN", "/usr/bin/google-chrome")
 
-    # Explicitly provide ChromeDriver path
-    service = Service("/usr/local/bin/chromedriver")
+    # Explicitly set the path to ChromeDriver
+    service = Service(os.getenv("CHROMEDRIVER_PATH", "/usr/local/bin/chromedriver"))
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     url = f"https://finance.yahoo.com/quote/{ticker}/earnings"
