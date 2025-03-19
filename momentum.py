ba8629ea3dd62a0bd92645ef9f57207b465b0e10
max-carlo <max.carlo@mailbox.org>
2025-03-19 12:30:45 +0100

    Update momentum.py

--- momentum.py
+++ momentum.py
@@ -2,28 +2,28 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import time
 
 @st.cache_data
 def get_earnings_data(ticker):
-    options = webdriver.ChromeOptions()
+    options = Options()
+    options.binary_location = "/usr/bin/google-chrome"
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.binary_location = "/usr/bin/google-chrome"
-
+    
     service = Service(executable_path="/usr/local/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=options)
 
-    url = f"https://finance.yahoo.com/quote/{ticker}/"
+    url = f"https://finance.yahoo.com/quote/{ticker}/earnings"
     driver.get(url)
-    html = driver.page_source
+    time.sleep(5)
+    data = driver.page_source
     driver.quit()
-
-    return html
+    return data
 
 st.title("Earnings Whispers Scraper")
 ticker = st.text_input("Enter stock ticker:", "AAPL")
-
 if st.button("Fetch Data"):
     data = get_earnings_data(ticker)
-    st.text_area("Earnings Data", data[:1000], height=500)
+    st.text_area("Earnings Data", data[:1000])
