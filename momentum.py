cf8738e19c8bc430d1ec7cf1387168b65f72a369
max-carlo <max.carlo@mailbox.org>
2025-03-19 22:17:31 +0100

    Update momentum.py

--- momentum.py
+++ momentum.py
@@ -5,11 +5,15 @@ from selenium.webdriver.chrome.options import Options
 import os
 
 def get_earnings_data(ticker):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = os.getenv("CHROME_BIN", "/usr/bin/google-chrome")
+   chrome_options = Options()
+chrome_options.add_argument("--headless=new")  # Nutzt den neuesten Headless-Modus
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--remote-debugging-port=9222")  # Setzt DevTools Port
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # Verhindert Bot-Erkennung
+chrome_options.add_argument("--window-size=1920,1080")  # Falls ein Fenster benötigt wird
+chrome_options.binary_location = os.getenv("CHROME_BIN", "/usr/bin/google-chrome")
 
     # Explicitly set the path to ChromeDriver
     service = Service(os.getenv("CHROMEDRIVER_PATH", "/usr/local/bin/chromedriver"))
